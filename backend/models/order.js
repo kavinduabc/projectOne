6ec3f960aefc289ec0d1b0e398b7ce66df0d4511
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-08 15:44:34 +0530

    order model

---
+++ backend/models/order.js
@@ -0,0 +1,46 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true,
+    },
+    email: {
+        type:String,
+        required: true,
+    },
+    address: {
+        city: {
+            type:String,
+            required: true,
+        },
+        country: String,
+        state: String,
+        zipcode: String,
+    },
+    phone: {
+        type:Number,
+        required: true,
+    },
+    productIds: [
+        {
+            productId: {
+                type: String,
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+            }
+        }
+    ],
+    totalPrice: {
+        type:Number,
+        required: true,
+    }
+}, {
+    timestamps: true,
+});
+
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
