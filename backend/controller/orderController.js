a1b3c8ee13f823645497261b0eb66fe5f1ccf752
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-12 22:40:41 +0530

    create order controller

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -1,4 +1,4 @@
-const Order = require('./order.model');
+const Order = require('../models/order.js');
 
 const createOrder = async (req, res) => {
     try{
@@ -6,7 +6,6 @@ const createOrder = async (req, res) => {
         const savedOrder = await newOrder.save();
         res.status(200).json(savedOrder);
     } catch(error) {
-        res.status(500).json({message:"Failed to create order"});
     }
 };
 
