bfaea055c3f4466c3cd57ed97cdd45f3c350a885
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-12 22:43:34 +0530

    sort orders by timestamp

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -25,7 +25,7 @@ const getOrdersByEmail = async (req, res) => {
 
 const getAllOrders = async (req, res) => {
     try {
-        const orders = await Order.find();
+        const orders = await Order.find().sort({ createdAt: -1 });
         res.status(200).json(orders);
     } catch (error) {
         res.status(500).json({ message: "Failed to fetch orders" });
