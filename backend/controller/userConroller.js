1dab9531f54a976ef8c17025b5145f3324d9ace6
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-03-12 14:58:33 +0530

    continue userController ffunctions

--- backend/controller/userConroller.js
+++ backend/controller/userConroller.js
@@ -33,3 +33,34 @@ export async function rejisterUser(req,res){
         //})
    // })
 }
+
+
+export  function loginUser(req,res){
+
+    const data = req.body;
+    
+    User.findOne({
+        email : data.email
+    }).then(
+        (user) =>{
+            if(user == null){
+                res.status(404).json({
+                    error:"User not found"
+                })
+            }else{
+                const isPasswordCorrect = bcrypt.compareSync(data.password,user.password)
+                
+                if(isPasswordCorrect){
+                    const token = jwt.sign({
+                        
+                        firstName : user.firstName,
+                        lastName : user.lastName,
+                        email : user.email,
+                    })
+                }
+            }
+        }
+    )
+
+
+}
