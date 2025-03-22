435e3c143898740766b300fe35be1aaa67490b2a
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-23 01:37:56 +0530

    order routes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -8,7 +8,14 @@ router.post("/",  createOrder);
 // get order by email
 router.get('/email/:email', getOrdersByEmail);
 
+// get all orders
+router.get('/', getAllOrders);
 
+// delete a order
+router.delete("/delete/:id", deleteOrder);
+
+//update a book
+router.put("/update/:id", updateOrder);
 
 
 module.exports = router;
