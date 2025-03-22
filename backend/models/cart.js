74ded5078ae1a3e4b93215d3368fcb57b5a75e77
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-23 01:31:00 +0530

    cart schema

---
+++ backend/models/cart.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Cart Schema
+const cartSchema = new Schema({
+    ItemId: String,
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+        minlength: 3
+    }
+    }
+});
+
