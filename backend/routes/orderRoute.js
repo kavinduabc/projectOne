180fa97804a6717c6544a96c330b582a65c1f574
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-23 01:36:56 +0530

    changes

---
+++ backend/routes/orderRoute.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const { createOrder, getOrdersByEmail, getAllOrders, deleteOrder, updateOrder } = require('../controller/orderController
+const router = express.Router();
+
