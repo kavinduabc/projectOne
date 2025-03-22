7c7f8bfb975a683b71238de00636f5fb309fd599
Hewa Seelawansha <135198286+HewaSeelawansha@users.noreply.github.com>
2025-03-23 01:37:46 +0530

    order routes

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,4 +1,14 @@
 const express = require('express');
-const { createOrder, getOrdersByEmail, getAllOrders, deleteOrder, updateOrder } = require('../controller/orderController
+const { createOrder, getOrdersByEmail, getAllOrders, deleteOrder, updateOrder } = require('../controller/orderController')
 const router = express.Router();
 
+// create a new order
+router.post("/",  createOrder);
+
+// get order by email
+router.get('/email/:email', getOrdersByEmail);
+
+
+
+
+module.exports = router;
