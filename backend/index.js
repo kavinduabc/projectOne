5e1374a50d8853e8d5dcfdf994c6e38f3bd59d66
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-03-27 11:56:33 +0530

    port number is change 3000 to 4000

--- backend/index.js
+++ backend/index.js
@@ -47,7 +47,7 @@ app.use("/api/user",userRouter)
 
 
 //** port */
-app.listen(3000,()=>{
-    console.log("server is running on port 3000")
+app.listen(4000,()=>{
+    console.log("server is running on port 4000")
 })
 
