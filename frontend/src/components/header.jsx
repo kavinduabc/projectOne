035cfdc67425186851d63782ce328a105f9d8822
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-01 22:51:24 +0530

    import react icons

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -2,6 +2,12 @@ import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-   <div></div>
+    <header className="bg-white  w-full flex justify-center items-center ">
+      <div className="">Dias Family</div>
+      <div>Search</div>
+      <div>card</div>
+   
+
+    </header>
   );
 }
