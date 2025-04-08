8ae1ac528e623400bc671416ec72f5b9f9b47ced
kavinduabc <132426449+kavinduabc@users.noreply.github.com>
2025-04-08 14:11:10 +0530

    start header

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,11 +1,16 @@
 import { Link } from "react-router-dom";
+import { FaSearch } from "react-icons/fa";
+import { FaUser} from "react-icons/fa"
+import { FaIdCard} from "react-icons/fa"
 
 export default function Header() {
   return (
     <header className="bg-white  w-full flex justify-center items-center ">
-      <div className="">Dias Family</div>
+      <div className="text-purple-800 font-medium text-3xl">Dias Family</div>
       <div>Search</div>
-      <div>card</div>
+      <div>
+
+      </div>
    
 
     </header>
